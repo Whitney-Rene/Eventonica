f3125d5d16cbaed978d1646957cea9a1f0bf8bf7
Whitney-Rene <whtnchambers76@gmail.com>
2023-09-13 08:43:25 -0400

    adds a handlePostReq function

--- client/src/components/eventsList.jsx
+++ client/src/components/eventsList.jsx
@@ -1,42 +1,51 @@
 //imports react hooks and 2 comps
 import { useState, useEffect } from "react";
 import EventCard from "./eventCard";
+import AddEvent from "./addEventForm";
 import CardGroup from 'react-bootstrap/CardGroup';
 
 
 function EventsList() {
-    //state for events
-    const [events, setEvents] = useState([]);
+  //state for events
+  const [events, setEvents] = useState([]);
 
-    //call api
-    // const getRequest = () => {
-    //   fetch("http://localhost:8080/api/events")
-    //   .then((response) => response.json())
-    //   .then(events => {
-    //     setEvents(events); 
-    //     console.log('Events fetched...', events);
-    //     });
-    // }
+  //call api
+  const getRequest = async () => {
+    const response = await fetch('http://localhost:8080/api/events');
+    const events = await response.json();
+    setEvents(events);
+    console.log('Events fetched...', events);
+  }
 
-    //call api
-    const getRequest = async () => {
-      const response = await fetch ('http://localhost:8080/api/events');
-      const events = await response.json();
-      setEvents(events);
-      console.log('Events fetched...', events);
-    }
+  const handlePostRequest = (data) => {
+    fetch("http://localhost:8080/api/events", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        console.log("Inside the post line 28", data)
+        setEvents([...events, data])
+      })
+  }
 
-    //calls this function, when the page loads
-    useEffect(() => {getRequest()}, []);
+  //calls this function, when the page loads
+  useEffect(() => { getRequest() }, []);
 
   return (
+    <>
     <div>
-    <CardGroup className="Events">
-            {events.map(event =>
-            <EventCard key={event.id} title={event.title} location={event.location} eventdate={event.eventtime}/>
-            )}
-    </CardGroup>
+      <CardGroup className="Events">
+        {events.map(event =>
+          <EventCard key={event.id} title={event.title} location={event.location} eventdate={event.eventtime} />
+        )}
+      </CardGroup>
     </div>
+    <div>
+      <AddEvent submit={handlePostRequest}/>
+    </div>
+    </>
   );
 }
 
